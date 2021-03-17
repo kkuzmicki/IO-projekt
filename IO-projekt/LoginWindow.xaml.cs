0390fa026f61970b628fb1b3be33a94a09dc404d
kkuzm <kkuzm@DESKTOP-C0T2HAB>
2021-03-17 14:01:24 +0100

    zmiana nazw

---
+++ IO-projekt/LoginWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using FirebirdSql.Data.FirebirdClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IO_projekt
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        int hour;
+        FbConnection connection;
+        public LoginWindow()
+        {
+            InitializeComponent();
+            WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
+            hour = DateTime.Now.Hour;
+            if (hour >= 18 || hour <= 6)
+            {
+                welcomeL.Text = "Dobry wieczór!";
+            }
+
+            FbConnectionStringBuilder csb = new FbConnectionStringBuilder();
+            csb.DataSource = "localhost";
+            csb.Port = 3050;
+            csb.Database = @"C:\bazy\IO.FDB";
+            csb.UserID = "SYSDBA";
+            csb.Password = "masterkey";
+            csb.ServerType = FbServerType.Default;
+
+            connection = new FbConnection(csb.ToString());
+            connection.Open();
+        }
+
+        private void loginB_click(object sender, RoutedEventArgs e)
+        {
+            //using (var transaction = connection.BeginTransaction())
+            //{
+            //    using (var command = new FbCommand("select LOGIN, PASSWORD from USERS", connection, transaction))
+            //    {
+            //        using (var reader = command.ExecuteReader())
+            //        {
+            //            while (reader.Read())
+            //            {
+            //                IDataRecord record = reader;
+            //                if (loginTB.Text == (String)record[0] && passwordPB.Password == (String)record[1])
+            //                {
+            //                    MainWindow objSecondWindow = new MainWindow();
+            //                    this.Close();
+            //                    objSecondWindow.Show();
+            //                }
+            //            }
+            //        }
+            //    }
+            //}
+
+            var command2 = new FbCommand("select COUNT(*) from PRACOWNICY where LOGIN = '" + loginTB.Text + "' AND HASLO = '" + passwordPB.Password + "'", connection);
+            Int32 count = (Int32)command2.ExecuteScalar();
+            if(count > 0)
+            {
+                MainWindow objSecondWindow = new MainWindow();
+                this.Close();
+                objSecondWindow.Show();
+            } 
+            else
+            {
+                errorTB.Text = "Błędne dane";
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
--- IO-projekt/MainWindow.xaml.cs
+++ IO-projekt/MainWindow.xaml.cs
@@ -1,9 +1,5 @@
-﻿using FirebirdSql.Data.FirebirdClient;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Data;
-using System.Data.SqlClient;
-using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,75 +10,42 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace IO_projekt
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Logika interakcji dla klasy MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-        int hour;
-        FbConnection connection;
         public MainWindow()
         {
             InitializeComponent();
             WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
-            hour = DateTime.Now.Hour;
-            if (hour >= 18 || hour <= 6)
-            {
-                welcomeL.Text = "Dobry wieczór!";
-            }
-
-            FbConnectionStringBuilder csb = new FbConnectionStringBuilder();
-            csb.DataSource = "localhost";
-            csb.Port = 3050;
-            csb.Database = @"C:\bazy\IO.FDB";
-            csb.UserID = "SYSDBA";
-            csb.Password = "masterkey";
-            csb.ServerType = FbServerType.Default;
-
-            connection = new FbConnection(csb.ToString());
-            connection.Open();
         }
 
-        private void loginB_click(object sender, RoutedEventArgs e)
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
         {
-            //using (var transaction = connection.BeginTransaction())
-            //{
-            //    using (var command = new FbCommand("select LOGIN, PASSWORD from USERS", connection, transaction))
-            //    {
-            //        using (var reader = command.ExecuteReader())
-            //        {
-            //            while (reader.Read())
-            //            {
-            //                IDataRecord record = reader;
-            //                if (loginTB.Text == (String)record[0] && passwordPB.Password == (String)record[1])
-            //                {
-            //                    SecondWindow objSecondWindow = new SecondWindow();
-            //                    this.Close();
-            //                    objSecondWindow.Show();
-            //                }
-            //            }
-            //        }
-            //    }
-            //}
 
-            var command2 = new FbCommand("select COUNT(*) from PRACOWNICY where LOGIN = '" + loginTB.Text + "' AND HASLO = '" + passwordPB.Password + "'", connection);
-            Int32 count = (Int32)command2.ExecuteScalar();
-            if(count > 0)
-            {
-                SecondWindow objSecondWindow = new SecondWindow();
-                this.Close();
-                objSecondWindow.Show();
-            } 
-            else
-            {
-                errorTB.Text = "Błędne dane";
-            }
-            Console.WriteLine(count);
         }
+
+        private void btnEdit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void btnBook_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void btnReturn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
     }
 }
