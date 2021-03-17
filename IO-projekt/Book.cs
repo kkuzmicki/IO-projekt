11fa8af128bddcc6bb99c1d7718c44ae77a52318
kkuzm <kkuzm@DESKTOP-C0T2HAB>
2021-03-17 19:07:55 +0100

    KK

---
+++ IO-projekt/Book.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IO_projekt
+{
+    class Book
+    {
+        public int ID_KSIAZKA { get; set; }
+        public string TYTUL { get; set; }
+        public int ID_KATEGORIA { get; set; }
+        public string KATEGORIA { get; set; }
+        public int ID_AUTOR { get; set; }
+        public string AUTOR { get; set; }
+        public int ID_WYDAWNICTWO { get; set; }
+        public string WYDAWNICTWO { get; set; }
+        public int ROK_WYDANIA { get; set; }
+        public int ILOSC { get; set; }
+
+        public Book(int ID_KSIAZKA, string TYTUL, int ID_KATEGORIA, string KATEGORIA, int ID_AUTOR, string AUTOR, int ID_WYDAWNICTWO, 
+            string WYDAWNICTWO, int ROK_WYDANIA, int ILOSC)
+        {
+            this.ID_KSIAZKA = ID_KSIAZKA;
+            this.TYTUL = TYTUL;
+            this.ID_KATEGORIA = ID_KATEGORIA;
+            this.KATEGORIA = KATEGORIA;
+            this.ID_AUTOR = ID_AUTOR;
+            this.AUTOR = AUTOR;
+            this.ID_WYDAWNICTWO = ID_WYDAWNICTWO;
+            this.WYDAWNICTWO = WYDAWNICTWO;
+            this.ROK_WYDANIA = ROK_WYDANIA;
+            this.ILOSC = ILOSC;
+        }
+    }
+}
--- IO-projekt/MainWindow.xaml.cs
+++ IO-projekt/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using FirebirdSql.Data.FirebirdClient;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,10 +21,45 @@ namespace IO_projekt
     /// </summary>
     public partial class MainWindow : Window
     {
+        FbConnection connection;
         public MainWindow()
         {
             InitializeComponent();
             WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
+            FbConnectionStringBuilder csb = new FbConnectionStringBuilder();
+            csb.DataSource = "localhost";
+            csb.Port = 3050;
+            csb.Database = @"C:\bazy\IO.FDB";
+            csb.UserID = "SYSDBA";
+            csb.Password = "masterkey";
+            csb.ServerType = FbServerType.Default;
+
+            connection = new FbConnection(csb.ToString());
+            connection.Open();
+            Book test = new Book(1, "GIMP. Poradnik", 1, "Poradniki", 1, "Kamil Kuźmicki", 1, "Znak", 2021, 2);
+            Books.Items.Add(test);
+
+            using (var transaction = connection.BeginTransaction())
+            {
+                using (var command = new FbCommand("select ID_KSIAZKA, TYTUL, ks.ID_KATEGORIA, KATEGORIA, ks.ID_AUTOR, IMIE || ' ' || NAZWISKO, ks.ID_WYDAWNICTWO, WYDAWNICTWO, " +
+                    "ROK_WYDANIA, ILOSC from KSIAZKA ks " +
+                    "inner join KATEGORIE ka on ks.ID_KATEGORIA = ka.ID_KATEGORIA " +
+                    "inner join AUTORZY a on ks.ID_AUTOR = a.ID_AUTOR " +
+                    "inner join WYDAWNICTWA w on ks.ID_WYDAWNICTWO = w.ID_WYDAWNICTWO", connection, transaction))
+                {
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            IDataRecord record = reader;
+                            Console.WriteLine("heh");
+                            Book tmp = new Book((int)record[0], (string)record[1], (int)record[2], (string)record[3], (int)record[4], (string)record[5], 
+                                (int)record[6], (string)record[7], (int)record[8], (int)record[9]);
+                            Books.Items.Add(tmp);
+                        }
+                    }
+                }
+            }
         }
 
         private void btnAdd_Click(object sender, RoutedEventArgs e)
